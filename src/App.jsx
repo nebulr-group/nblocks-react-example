6ec40a9db6b6322c6b8251697152933ea1a32e31
Oscar Söderlund <oscar@nebulr.group>
2023-02-07 18:48:51 +0100

    Tutorial end

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  useLocation
+} from 'react-router-dom';
+import Sidebar from './partials/Sidebar';
 import "./App.css";
 import './charts/ChartjsConfig';
-import Dashboard from "./pages/Dashboard";
+
+// Import pages
+import AppRoutes from './Routes';
 
 import { NblocksProvider } from "@nebulr-group/nblocks-react";
 
 function App() {
+
+  const location = useLocation();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <NblocksProvider config={{devMode: true}}>
+    <NblocksProvider config={{devMode: true, spa: false, handoverRoute: "/dashboard"}}>
       <div className="w-full flex h-screen">
+        {['/dashboard', '/analytics', '/user/list'].includes(location.pathname) && (
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        )}
         <div className="overflow-y-auto w-full flex justify-center nblocks-router">
-          <Dashboard />
+          <AppRoutes />
         </div>
       </div>
     </NblocksProvider>
