5856f9cd9c149a15ff40914ea15d6c645a595a43
Nemanja Manojlovic <nemanja@nebulr.group>
2023-02-07 15:58:25 +0100

    Display example components on the dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import {
-  useLocation
-} from 'react-router-dom';
-import Sidebar from './partials/Sidebar';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Sidebar from "./partials/Sidebar";
 import "./App.css";
-import './charts/ChartjsConfig';
+import "./charts/ChartjsConfig";
 
 // Import pages
-import AppRoutes from './Routes';
+import AppRoutes from "./Routes";
 
 import { NblocksProvider } from "@nebulr-group/nblocks-react";
 
 function App() {
-
   const location = useLocation();
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <NblocksProvider config={{signup: true, passwordValidation: false}}>
+    <NblocksProvider config={{ signup: true, passwordValidation: false }}>
       <div className="w-full flex h-screen">
-        {['/dashboard', '/analytics'].includes(location.pathname) && (
+        {["/dashboard", "/analytics"].includes(location.pathname) && (
           <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         )}
         <div className="overflow-y-auto w-full flex justify-center nblocks-router">
           <AppRoutes />
         </div>
       </div>
-      
     </NblocksProvider>
   );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import WelcomeBanner from '../partials/dashboard/WelcomeBanner';
-import DashboardCard01 from '../partials/dashboard/DashboardCard01';
-import DashboardCard02 from '../partials/dashboard/DashboardCard02';
-import DashboardCard03 from '../partials/dashboard/DashboardCard03';
-import DashboardCard04 from '../partials/dashboard/DashboardCard04';
-import DashboardCard05 from '../partials/dashboard/DashboardCard05';
+import React from "react";
+import WelcomeBanner from "../partials/dashboard/WelcomeBanner";
+import ExampleHeading from "../components/ExampleHeading";
+import UserListButton from "../components/UserListButton";
+import NotificationForm from "../components/NotificationForm";
 // import '../css/style.css';
 
-
 function Dashboard() {
-
   return (
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-
-        <main>
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-            {/* Welcome banner */}
-            <WelcomeBanner />
-          </div>
-        </main>
-
-      </div>
+    <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+      <main>
+        <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          {/* Welcome banner */}
+          <WelcomeBanner />
+          <ExampleHeading className={"text-2xl"} />
+          <UserListButton>List Tenant Users</UserListButton>
+          <NotificationForm />
+        </div>
+      </main>
+    </div>
   );
 }
 
