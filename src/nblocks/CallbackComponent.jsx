02e5002f999435e4ed25be8d4477d24e1e30cc83
Oscar Söderlund <oscar@nebulr.group>
2023-11-06 18:37:06 +0100

    Adapting to new quickstart

--- src/nblocks/CallbackComponent.jsx
+++ src/nblocks/CallbackComponent.jsx
@@ -6,36 +6,29 @@ import {APPLICATION_ID, NBLOCKS_AUTH } from "./Globals";
 // Users will get back to this component after finishing login
 export default function CallbackComponent() {
 
-  const location = useLocation();
-  const urlSearch = new URLSearchParams(location.search);
-  const code = urlSearch.get("code");
-
   // Replace this with your own APP ID
   const APP_ID = APPLICATION_ID;
 
+  const location = useLocation();
   const [accessToken, setAccessToken] = useState();
 
   useEffect(() => {
+    const code = new URLSearchParams(location.search).get("code");
     if (code) {
       handleCallback(code);
     }
-  }, [code]);
+  }, []);
 
   const handleCallback = async (code) => {
-    // Make the API call to Nblocks
+    // Get tokens
     const tokens = await fetch(`${NBLOCKS_AUTH}/token/code/${APP_ID}`,
       {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          code: code,
-        }),
+        method: "POST", headers: { "Content-Type": "application/json", },
+        body: JSON.stringify({ code }),
       }
     ).then(res => res.json());
 
-    // Verify the result using public keys from Nblocks JWKS
+    // Verify the tokens result using public keys from Nblocks JWKS
     const { access_token, refresh_token, id_token } = tokens;
     const { payload } = await jwtVerify(
       access_token, createRemoteJWKSet(
@@ -44,18 +37,14 @@ export default function CallbackComponent() {
     );
 
     // Store the result in component state and localstorage
-    setAccessToken(payload);
     window.localStorage.setItem('access_token', access_token);
     window.localStorage.setItem('refresh_token', refresh_token);
     window.localStorage.setItem('id_token', id_token);
-
-    console.log("Successfully exchanged code for tokens");
+    setAccessToken(payload);
+    console.log("User access token", payload);
   };
 
   if (accessToken)
-    if (accessToken.shouldSelectPlan || accessToken.shouldSetupPayments)
-      return (<Navigate to={"/selectPlan"}/>);
-    else
       return (<Navigate to={"/"}/>);
   else
     return (<p>Not logged in</p>);
