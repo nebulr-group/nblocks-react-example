19527bfccdc0451606f54fed3111e710007c3d8c
Oscar Söderlund <oscar@nebulr.group>
2024-06-16 01:36:22 +0200

    Sidebar is baked in Layout wrapper so that it easier to show these only for logged in users when adding ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import Sidebar from './partials/Sidebar';
+import React from 'react';
 import "./App.css";
 import './charts/ChartjsConfig';
 
@@ -8,15 +7,8 @@ import AppRoutes from './Routes';
 
 function App() {
 
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
-    <div className="w-full flex h-screen">
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      <div className="overflow-y-auto w-full flex justify-center nblocks-router">
-        <AppRoutes />
-      </div>
-    </div>
+    <AppRoutes />
   );
 }
 
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,18 +3,22 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import Analytics from "./pages/Analytics";
 import Dashboard from "./pages/Dashboard";
+import Layout from "./partials/Layout";
 
 function AppRoutes() {
+  
   return (
     <Routes>
       <Route path="*" element={
-              <Routes>
-                <Route exact path="/dashboard" element={<Dashboard />}/>
-                <Route exact path="/analytics" element={<Analytics />}/>
-                <Route path="*" element={<Navigate to="/dashboard" replace={true} />}/>
-              </Routes>
-          }
-        />
+        <Layout>
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />}/>
+            <Route exact path="/analytics" element={<Analytics />}/>
+            <Route path="*" element={<Navigate to="/dashboard" replace={true} />}/>
+          </Routes>
+        </Layout>
+      }
+      />
     </Routes>
   );
 }
